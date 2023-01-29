d7a41cd59c7253fad2c60c609275a8573eb37099
Aknur Seidazym <pok.pokova@bk.ru>
2023-01-29 15:02:39 +0600

    loop

---
+++ week2/for/ex.py
@@ -0,0 +1,26 @@
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+'''
+
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+'''
+
+'''
+for i in range(6):
+    print(i)
+'''
+
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+'''
